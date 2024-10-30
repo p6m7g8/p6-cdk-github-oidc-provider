260b80e81425e00f7266d64392592d2ff3cd67b9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-30 16:40:44 -0400

    fix: this.node.tryFindChild

--- src/p6-cdk-github-oidc-provider.ts
+++ src/p6-cdk-github-oidc-provider.ts
@@ -1,4 +1,3 @@
-import { Stack } from 'aws-cdk-lib'
 import * as iam from 'aws-cdk-lib/aws-iam'
 import { Construct } from 'constructs'
 
@@ -12,17 +11,14 @@ export class P6CDKGithubOidcProvider extends Construct {
   constructor(scope: Construct, id: string, props: IP6CDKGithubOidcProviderProps) {
     super(scope, id)
 
-    const providerArn = `arn:aws:iam::${Stack.of(this).account}:oidc-provider/token.actions.githubusercontent.com`
-
-    const existingOidcProvider = iam.OpenIdConnectProvider.fromOpenIdConnectProviderArn(this, 'ExistingOidcProvider', providerArn)
-
-    // Create the OIDC Provider for GitHub
-    const oidcProvider = existingOidcProvider ?? new iam.OpenIdConnectProvider(this, 'GithubOidc', {
-      url: 'https://token.actions.githubusercontent.com',
-      clientIds: ['sts.amazonaws.com'],
-      thumbprints: ['ffffffffffffffffffffffffffffffffffffffff'],
-    })
-
+    let oidcProvider = this.node.tryFindChild('GithubOidc') as iam.OpenIdConnectProvider
+    if (!oidcProvider) {
+      oidcProvider = new iam.OpenIdConnectProvider(this, 'GithubOidc', {
+        url: 'https://token.actions.githubusercontent.com',
+        clientIds: ['sts.amazonaws.com'],
+        thumbprints: ['ffffffffffffffffffffffffffffffffffffffff'],
+      })
+    }
     // Create the IAM Role for GitHub OIDC Authentication
     const role = new iam.Role(this, 'GithubOidcRole', {
       assumedBy: new iam.FederatedPrincipal(
